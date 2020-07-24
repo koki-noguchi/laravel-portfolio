c20997c3143b7b7a7d6d67ab616404c1449799ab
koki_noguchi <koki.noguchi0202@gmail.com>
2020-07-25 08:57:08 +0900

    [add]vuetifyの導入

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,14 +4,19 @@ import router from './router'
 import store from './store'
 import Croppa from 'vue-croppa'
 import 'vue-croppa/dist/vue-croppa.css'
+import '@mdi/font/css/materialdesignicons.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
 import App from './App.vue'
 
+Vue.use(Vuetify);
+Vue.use(Croppa);
 const createApp = async () => {
   await store.dispatch('auth/currentUser')
 
-  Vue.use(Croppa)
-
   new Vue({
+  vuetify: new Vuetify(),
   el: '#app',
   router,
   store,
