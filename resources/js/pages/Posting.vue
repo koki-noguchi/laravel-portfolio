4e8a13ff578488dfcf8687ed93da254f4e5702e5
koki_noguchi <koki.noguchi0202@gmail.com>
2020-07-27 10:54:42 +0900

    [fix]メッセージ募集ページのデザインを変更

--- resources/js/pages/Posting.vue
+++ resources/js/pages/Posting.vue
@@ -1,14 +1,65 @@
 <template>
-  <div class="post-form">
-    <h2 class="title">メッセージを募集する</h2>
-    <form class="form" @submit.prevent="createPost">
-      <input class="form__item" type="text" v-model="post_title" placeholder="タイトル">
-      <input class="form__item" type="password" v-model="post_password" placeholder="パスワード">
-      <input class="form__item" type="number" v-model="min_number" placeholder="最低人数">
-      <input class="form__item" type="number" v-model="max_number" placeholder="最大人数">
-      <div class="form__button">
-        <button type="submit" class="button button--inverse">送信</button>
-      </div>
+  <div class="post-form text-center">
+    <div class="h2">メッセージを募集する</div>
+    <form class="form mt-10" @submit.prevent="createPost">
+      <v-col cols="12">
+          <v-text-field
+            v-model="post_title"
+            :rules="rules"
+            counter
+            maxlength="100"
+            clearable
+            label="タイトル"
+          ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+          <v-text-field
+            v-model="post_password"
+            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="show1 ? 'text' : 'password'"
+            @click:append="show1 = !show1"
+            clearable
+            label="パスワード"
+          ></v-text-field>
+      </v-col>
+      <v-col cols="6">
+          <v-text-field
+            v-model="max_number"
+            type="number"
+            clearable
+            label="最大人数"
+          >
+            <template v-slot:prepend>
+              <v-tooltip
+                bottom
+              >
+                <template v-slot:activator="{ on }">
+                  <v-icon v-on="on">mdi-help-circle-outline</v-icon>
+                </template>
+                メッセージ数が最大人数分に達すると募集が締めきられます。
+              </v-tooltip>
+            </template>
+          </v-text-field>
+      </v-col>
+      <v-file-input
+        v-model="files"
+        placeholder="Upload your images"
+        label="画像のアップロード"
+        multiple
+        prepend-icon="mdi-paperclip"
+        show-size
+      >
+        <template v-slot:selection="{ text }">
+          <v-chip
+            small
+            label
+            color="primary"
+          >
+            {{ text }}
+          </v-chip>
+        </template>
+      </v-file-input>
+        <v-btn type="submit" width="160" class="ma-2 mt-10" outlined color="pink lighten-1">送信</v-btn>
     </form>
   </div>
 </template>
@@ -21,6 +72,8 @@ export default {
             post_password: '',
             min_number: '',
             max_number: '',
+            show1: false,
+            files: [],
         }
     },
     methods: {
