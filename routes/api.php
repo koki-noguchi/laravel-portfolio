3416ddf647e75abef370a6bc950cdb50ec3ce35c
koki_noguchi <koki.noguchi0202@gmail.com>
2020-08-08 09:21:14 +0900

    [add]2回フォローできないテストを追加・フォローを外せるテストとルーティングを追加

--- routes/api.php
+++ routes/api.php
@@ -38,3 +38,4 @@ Route::delete('/reply/{id}', 'ReplyController@delete')->name('reply.delete');
 Route::delete('/photo/{id}', 'PhotoController@delete')->name('photo.delete');
 Route::post('/post/{id}/photo', 'PhotoController@create')->name('photo.create');
 Route::put('/users/{id}/follow', 'UserController@follow')->name('follow.add');
+Route::delete('/users/{id}/follow', 'UserController@deleteFollow')->name('follow.delete');
--- tests/Feature/FollowApiTest.php
+++ tests/Feature/FollowApiTest.php
@@ -36,4 +36,36 @@ class FollowApiTest extends TestCase
 
         $this->assertEquals(1, $this->user->followings()->count());
     }
+
+    /**
+     * @test
+     */
+    public function should_同じユーザーを2回フォローできない()
+    {
+        $param = ['id' => $this->follow_user->id];
+        $this->actingAs($this->user)->json('PUT', route('follow.add', $param));
+        $this->actingAs($this->user)->json('PUT', route('follow.add', $param));
+
+        $this->assertEquals(1, $this->user->followings()->count());
+    }
+
+    /**
+     * @test
+     */
+    public function should_フォローを外せる()
+    {
+        $this->user->followings()->attach($this->follow_user->id,);
+
+        $response = $this->actingAs($this->user)
+            ->json('DELETE', route('follow.delete', [
+                'id' => $this->follow_user->id,
+            ]));
+
+        $response->assertStatus(200)
+            ->assertJsonFragment([
+                'followee_id' => $this->follow_user->id,
+            ]);
+
+        $this->assertEquals(0, $this->user->followings()->count());
+    }
 }
