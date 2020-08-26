65656f568f34d9039480ff5247960306f8d91898
koki_noguchi <koki.noguchi0202@gmail.com>
2020-08-26 11:23:03 +0900

    [fix]web.phpのルーティングを一部修正

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{any?}', fn () => view('index'))->where('any',  '.+');
+Route::get('/{any?}', function () {return view('index');})->where('any',  '.+');
 
